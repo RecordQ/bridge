9e6cf105ef90ca8925dcf0e7e1b0e7c388040282
Darya Kardo <daryarecord@gmail.com>
2025-07-11 17:33:43 +0000

    in the main page when making it too small the page gets truncated to the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       </head>
       <body className={cn("min-h-screen bg-background font-body antialiased")}>
         <ThreeScene />
-        <div className="relative flex min-h-screen flex-col">
+        <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden">
           <Header />
           <main className="flex-1">{children}</main>
           <Footer />
