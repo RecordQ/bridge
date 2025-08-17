40875e26f6b13c14ebee9615ad191a4f4eac380c
Darya Kardo <daryarecord@gmail.com>
2025-08-17 08:33:22 +0000

    remove the navbar

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -4,14 +4,12 @@
 import type { ReactNode } from "react";
 import { Chatbot } from "../Chatbot";
 import { ThreeScene } from "../ThreeScene";
-import { Header } from "./Header";
 
 export function PageLayout({ children }: { children: ReactNode }) {
     
     return (
         <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden">
             <ThreeScene />
-            <Header />
             <div className="pt-16">
                 {children}
             </div>
