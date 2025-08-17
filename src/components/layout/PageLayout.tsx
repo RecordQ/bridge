ec73db5be407792eab748997ec9b137c73452079
Darya Kardo <daryarecord@gmail.com>
2025-08-17 08:32:17 +0000

    its the same, remove the navbar i will tell you what to do next

--- src/components/layout/PageLayout.tsx
+++ src/components/layout/PageLayout.tsx
@@ -2,7 +2,6 @@
 "use client";
 
 import type { ReactNode } from "react";
-import { Header } from "./Header";
 import { Chatbot } from "../Chatbot";
 import { ThreeScene } from "../ThreeScene";
 
@@ -11,7 +10,6 @@ export function PageLayout({ children }: { children: ReactNode }) {
     return (
         <div className="relative flex min-h-screen w-full flex-col overflow-x-hidden">
             <ThreeScene />
-            <Header />
             {children}
             <Chatbot />
         </div>
